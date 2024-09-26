2747b2b36f3bab1171de153e2da0b0471e38daa0
juanppjaime <j.penaj@uniandes.edu.co>
2024-09-25 20:48:58 -0500

    Cambios taller

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import { IntlProvider } from 'react-intl';
+import localeEsMessages from "./locales/es";
+import localeEnMessages from "./locales/en";
 import JobsList from "./components/jobslist";
 
-ReactDOM.render(<JobsList />, document.getElementById("root"));
+const locale = navigator.language.startsWith('es') ? 'es-ES' : 'en-US';
+const messages = locale.startsWith('es') ? localeEsMessages : localeEnMessages;
+
+ReactDOM.render(
+  <IntlProvider locale={locale} messages={messages}>
+    <JobsList />
+  </IntlProvider>,
+  document.getElementById("root")
+);
