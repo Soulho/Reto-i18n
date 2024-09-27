02ddc3b6e5d7a7f385a7474cca7e57edd4b85a91
Daniel Fuentes <d.fuentese@uniandes.edu.co>
2024-09-27 17:31:19 -0500

    Reto internalizacion

--- src/components/jobslist.js
+++ src/components/jobslist.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import Job from "./job";
+import {FormattedMessage} from 'react-intl' ;
+import './jobslist.css';
 
 const JobsList = () => {
   const [offers] = useState([
@@ -10,6 +12,7 @@ const JobsList = () => {
       salary: 4.5,
       city: "Bogotá, Colombia",
       date: "2019-03-26",
+      visitas: 1500
     },
     {
       id: "0002",
@@ -18,6 +21,7 @@ const JobsList = () => {
       salary: 20,
       city: "Palo Alto, CA, USA",
       date: "2019-03-27",
+      visitas: 10500
     },
     {
       id: "0003",
@@ -26,20 +30,35 @@ const JobsList = () => {
       salary: 1,
       city: "Cali, Colombia",
       date: "2019-03-28",
+      visitas: 105000
     },
   ]);
+  const isSpanish = navigator.language.startsWith('es');
 
   return (
     <div>
       <table className="table">
-        <thead className="thead-dark">
+      <thead className={isSpanish ? "thead-light" : "thead-dark"}>
           <tr>
             <th scope="col">#</th>
-            <th scope="col">Position</th>
-            <th scope="col">Company</th>
-            <th scope="col">Salary</th>
-            <th scope="col">City</th>
-            <th scope="col">Publication date</th>
+            <th scope="col">
+                <FormattedMessage id="Position"/>
+            </th>
+            <th scope="col">
+                <FormattedMessage id="Company"/>
+            </th>
+            <th scope="col">
+                <FormattedMessage id="Salary"/>
+            </th>
+            <th scope="col">
+                <FormattedMessage id="City"/>
+            </th>
+            <th scope="col">
+                <FormattedMessage id="PublicationDate"/>
+            </th>
+            <th scope="col">
+              <FormattedMessage id="Views"/>
+            </th>
           </tr>
         </thead>
         <tbody>
